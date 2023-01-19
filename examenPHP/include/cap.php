4b5817904638bd9d4ea27b7889cfa1eecb3c6045
GUIXCODEART <guille040471@hotmail.com>
2023-01-19 08:39:01 +0100

    delete carpeta examen php

--- examenPHP/include/cap.php
+++
@@ -1,7 +0,0 @@
-<?php
-	echo "<img src=\"img/logophp.png\" alt=\"logo PHP\" class=\"fotocap\" />";
-
-	echo "<h1>Examen PHP 1a Avaluació: </h1>";
-
-	echo "<img src=\"img/logophp.png\" alt=\"logo PHP\" class=\"fotocap\" />";
-?>
--- examenPHP/include/peu.php
+++
@@ -1,7 +0,0 @@
-<?php
-		echo "<p>
-						IES Lluís Simarro<br/>
-						Entorns de Desenvolupament<br/>
-						Curs 2020/2021<br/>
-					</p>";
-?>
